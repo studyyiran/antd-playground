58fbf37ef92d70653e88dcc7de2f3d70015df0a0
yiranzi <570932296@qq.com>
2019-07-13 10:56:22 +0800

    upgrade: init

---
+++ src/pages/page1/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "./index.scss";
+export default function() {
+  return <div></div>;
+}
---
+++ src/router.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Page1 from "./pages/page1";
+
+function Router() {
+  return (
+    <div>
+      <Page1 />
+    </div>
+  );
+}
+
+export default Router;
